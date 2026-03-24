9c60b32f0c7e52e3a1de294a6d844612088d713c
Shatarudra Prakash Singh <vpatel4110@gmail.com>
2026-03-24 12:32:36 +0530

    Create App.tsx main application file with navigation setup

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = () => <div>Home Screen</div>;
+const DetailsScreen = () => <div>Details Screen</div>;
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
