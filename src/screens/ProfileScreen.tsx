ea7834891b1f2ae5d316b24887666bc6f7d1e4ac
Shatarudra Prakash Singh <vpatel4110@gmail.com>
2026-03-24 12:52:22 +0530

    Create ProfileScreen.tsx showing user profile and developer information

---
+++ src/screens/ProfileScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProfileScreen = () => {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Developer: Shatarudra</p>
+      <p>Instagram: <a href='https://instagram.com/shatarudra_92'>shatarudra_92</a></p>
+    </div>
+  );
+};
+
+export default ProfileScreen;
